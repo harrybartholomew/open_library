79b1cb3c54d2262dc67b1d2a565d1fea1e0440ca
Harry Bartholomew <harry.bartholomew@gmail.com>
2023-03-06 23:05:18 +0000

    Remove files

--- test.py
+++
@@ -1,7 +0,0 @@
-import pandas
-
-leganto_data = pandas.read_csv("example_leganto_list.csv")
-
-for i, r in leganto_data.iterrows():
-    x = r["Citation Title"].find("/")
-    print(r["Citation Title"][:x-1])
